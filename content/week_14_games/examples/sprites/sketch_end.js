58104313b0c69d1e65cdf5512755fca1ac579b1a
Justin Bakse <justin@tsfim.com>
2017-05-05 17:30:51 -0400

    sketch sprites

--- content/week_14_games/examples/sprites/sketch_end.js
+++ content/week_14_games/examples/sprites/sketch_end.js
@@ -13,10 +13,14 @@ function setup() {
     sprite1 = createSprite(200, 150, 100, 100);
     sprite1.addImage("main", kingImage);
     sprite1.scale = 4;
+    sprite1.addSpeed(1, 0);
+
 
     sprite2 = createSprite(400, 150, 100, 100);
     sprite2.addImage("main", guyImage);
     sprite2.scale = 4;
+    sprite2.mirrorX(-1);
+    // sprite2.mass = 2;
     sprite2.addSpeed(1, 180);
 }
 
@@ -24,5 +28,6 @@ function setup() {
 function draw() {
     background(50, 50, 80);
     noSmooth();
+    sprite1.bounce(sprite2);
     drawSprites();
 }
